5933c65b5bd7db64ce74fdf43367d24c7304f86d
mathew <mathew@mathews-MacBook-Pro.local>
2022-04-24 22:19:33 -0400

    fixed algo

--- grad_desc.py
+++ grad_desc.py
@@ -1,34 +1,30 @@
 import numpy as np
 
-def grad_descent(x,y,m1_guess,m0_guess,alpha0,init_step):
+def grad_descent(x,y,m1_guess,m0_guess,alpha0):
     # intitializes params
     m1 = m1_guess
     m0 = m0_guess
     alpha = alpha0
-    step1 = init_step
-    step0 = init_step
+   
 
+    
 
     # runs grad_desc algo
 
-    sse = 0
+    for k in range(0,10000):
 
-    for i in range(0,len(x)):
-        sse = sse + np.float_power((m1*x[i] + m0 - y[i]),2)
+        dm1 = 0
+        dm0 = 0
+        for i in range(0,len(x)):
+            
+            dm1 = x[i] * (m1*x[i] + m0 - y[i]) + dm1
+            dm0 = m1*x[i] + m0 - y[i] + dm0
+        
+        
+        
+        
+        m1  = m1 - alpha * dm1/len(x)
+        m0 = m0 - alpha * dm0/len(x)
 
-    m1_new = m1 - step1
-    m0_new = m0 - step0
 
-    sse_new = 0
-    for j in range(0,len(x)):
-        sse_new = sse_new + np.float_power((m1_new*x[i] + m0_new - y[i]),2)
-
-   
-    
-
-
-# function test
-x =[2.4,3.67,3,4]
-y = [.5,.7,.6,.8]
-
-print(grad_descent(x,y,3,1,1,.1))
+    return [m1,m0]
