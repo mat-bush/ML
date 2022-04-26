d4b297af8472e549d24ce7cf09f62eb5d8f10a80
mathew <mathew@mathews-MacBook-Pro.local>
2022-04-25 21:01:36 -0400

    added error detection and changed to class

--- grad_desc.py
+++ grad_desc.py
@@ -1,30 +1,34 @@
 import numpy as np
 
-def grad_descent(x,y,m1_guess,m0_guess,alpha0):
-    # intitializes params
-    m1 = m1_guess
-    m0 = m0_guess
-    alpha = alpha0
-   
+class grad_desc():
 
     
-
-    # runs grad_desc algo
-
-    for k in range(0,10000):
-
-        dm1 = 0
-        dm0 = 0
-        for i in range(0,len(x)):
-            
-            dm1 = x[i] * (m1*x[i] + m0 - y[i]) + dm1
-            dm0 = m1*x[i] + m0 - y[i] + dm0
+    def sin_var_lin_reg(self,x,y,m1_guess,m0_guess,alpha0):
+        # intitializes params
+        m1 = m1_guess
+        m0 = m0_guess
+        alpha = alpha0
+    
+        if len(x) != len(y):
+            print("Error: lenght 'x' does not equal lenght 'y'")
+        else:  
         
-        
-        
-        
-        m1  = m1 - alpha * dm1/len(x)
-        m0 = m0 - alpha * dm0/len(x)
+
+            # runs grad_desc algo
+
+            for k in range(0,10000):
+
+                dm1 = 0
+                dm0 = 0
+                for i in range(0,len(x)):
+                    dm1 = x[i] * (m1*x[i] + m0 - y[i]) + dm1
+                    dm0 = m1*x[i] + m0 - y[i] + dm0
+                
+                
+                
+                
+                m1  = m1 - alpha * dm1/len(x)
+                m0 = m0 - alpha * dm0/len(x)
 
 
-    return [m1,m0]
+            return [m1,m0]
